9714fa9e854e99fdac14730910562ab3f4b865f7
KidKuro <42434430+naruedonJan@users.noreply.github.com>
2022-09-03 03:47:34 +0700

    create PokemonProfileComponent

--- pokemon_angular/src/app/app-routing.module.ts
+++ pokemon_angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonCharactersComponent } from './pokemon-characters/pokemon-characters.component';
+import { PokemonProfileComponent } from './pokemon-profile/pokemon-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component: PokemonCharactersComponent},
+  {path:'profile' , component: PokemonProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pokemon_angular/src/app/app.module.ts
+++ pokemon_angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonCharactersComponent } from './pokemon-characters/pokemon-characters.component';
+import { PokemonProfileComponent } from './pokemon-profile/pokemon-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonCharactersComponent
+    PokemonCharactersComponent,
+    PokemonProfileComponent
   ],
   imports: [
     BrowserModule,
